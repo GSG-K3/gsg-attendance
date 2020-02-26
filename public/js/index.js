5cfd91223559e24325db1758a89c5e606b06e6f5
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-02-26 11:04:09 +0200

    add index html with index js #10

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  var today = new Date();
+  var date =
+    today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  document.getElementById('CurrentDate').value = date;
+
+  setInterval(function() {
+    var now = new Date();
+    document.getElementById('CurrentTime').value =
+      now.getHours() + ' : ' + now.getMinutes();
+  }, 1000);
+});
