5bdf86f0aa120f4ce8dcf15eae65219c43f776fe
Nujood <shawar.nujood@gmail.com>
2020-02-26 15:26:02 +0200

    Get mentors and course #13 #14

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,55 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
+  getCourse();
+
   var today = new Date();
   var date =
-    today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-  document.getElementById('CurrentDate').value = date;
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  document.getElementById("CurrentDate").value = date;
 
   setInterval(function() {
     var now = new Date();
-    document.getElementById('CurrentTime').value =
-      now.getHours() + ' : ' + now.getMinutes();
+    document.getElementById("CurrentTime").value =
+      now.getHours() + " : " + now.getMinutes();
   }, 1000);
 });
 
-apiCall('get', '/api/mentor/getMentorsData', response => {
-  console.log(response);
+const getMentors = () => {
+  apiCall("get", "/api/mentor/getMentorsData", response => {
+    console.log(response);
+    const mentorsList = document.getElementById("mentors_list");
+    const data = JSON.parse(response);
+    data.forEach(mentor => {
+      let option = document.createElement("option");
+      option.setAttribute("value", mentor.id);
+      option.innerHTML = mentor.name;
+      mentorsList.appendChild(option);
+    });
+  });
+};
+
+const getCourse = () => {
+  apiCall("get", "/api/course/getCourse", response => {
+    console.log("Courses:", response);
+    const courseList = document.getElementById("courses");
+    const data = JSON.parse(response);
+    data.forEach(course => {
+      let link = document.createElement("a");
+      link.setAttribute("href", course.id);
+      link.classList.add("course");
+      let icon = document.createElement("i");
+      icon.classList.add("fas", "fa-book", "fa-2x");
+      let courseName = document.createElement("h2");
+      courseName.innerHTML = course.name;
+      link.appendChild(icon);
+      link.appendChild(courseName);
+      courseList.appendChild(link);
+    });
+    getMentors();
+  });
+};
+
+let addAttendance = document.getElementById("addAttendance");
+addAttendance.addEventListener("click", e => {
+  e.preventDefault();
+  console.log("clicked !");
 });
--- src/app.js
+++ src/app.js
@@ -1,23 +1,34 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const path = require('path');
-const Mentorsquery = require('./database/queries/db_mentors');
+const express = require("express");
+const bodyparser = require("body-parser");
+const path = require("path");
+const Mentorsquery = require("./database/queries/db_mentors");
+const Coursequery = require("./database/queries/db_courses");
 
 const app = express();
 
-const courses = require('./controller/Courses');
+// const courses = require('./controller/Courses');
 
-app.set('port', process.env.PORT || 3001);
+app.set("port", process.env.PORT || 3001);
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.get('/api/mentor/getMentorsData', (req, res) => {
-  console.log('start api');
+app.get("/api/mentor/getMentorsData", (req, res) => {
+  console.log("start api");
   Mentorsquery(data => {
+    console.log("App mentors:", data);
     res.send(data);
   });
 });
-app.listen(app.get('port'), () => {
-  console.log(`the Server is Run in port ${app.get('port')}`);
+
+app.get("/api/course/getCourse", (req, res) => {
+  console.log("Done Catching the course");
+  Coursequery(data => {
+    console.log("App courses :", data);
+    res.send(data);
+  });
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`the Server is Run in port ${app.get("port")}`);
 });
